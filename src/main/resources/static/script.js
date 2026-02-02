ec061eea87bb272e8f237cf91b62e9cccd91b711
dheenadhayalanb240083-lgtm <dheenadhayalanb.240083@cse.ritchennai.edu.in>
2026-02-02 17:52:32 +0530

    Ensure static script.js uses relative API paths

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://jobtracker-backend-gl26.onrender.com';
+const API_URL = '';
 let currentUser = localStorage.getItem('currentUser');
 let currentEditingJobId = null;
 let allJobs = [];
